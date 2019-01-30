00a0de0061eb4d67158c7edafd1f48b981da763c
MaxShvets <maxim.shvets@gmail.com>
2019-01-31 00:49:07 +0200

    Created basic table component that displays provided data

---
+++ src/Table.vue
@@ -0,0 +1,31 @@
+<template>
+    <table>
+        <tr>
+            <th v-for="(titleText, titleKey) in data.titles" :key="titleKey">
+                {{ titleText }}
+            </th>
+        </tr>
+        <tr v-for="item in data.items" :key="item.name">
+            <td v-for="(_, propertyName) in data.titles" :key="propertyName">
+                {{ item[propertyName] }}
+            </td>
+        </tr>
+    </table>
+</template>
+
+<script>
+export default {
+    name: 'Table',
+    props: ['data']
+}
+</script>
+
+<style scoped>
+    table {
+        border-collapse: collapse;
+    }
+
+    td, th {
+        border: 1px solid #000;
+    }
+</style>
